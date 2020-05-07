d8cd334e7f18d5a81967a9b80902be8d53ca898f
nagarajumadamshetti <nagarajumadamshetti@gmail.com>
2020-05-07 14:59:40 +0530

    modified decode to verify to check if expired

--- controllers/jwtDecode.js
+++ controllers/jwtDecode.js
@@ -1,8 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const jwtDecode = async (req, res, next) => {
-    req.token = await jwt.decode(req.headers['token'])
-    next();
+    try {
+        req.token = jwt.verify(req.headers['token'], 'keyboard cat 4 ever');
+        next();
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = exports = jwtDecode;
